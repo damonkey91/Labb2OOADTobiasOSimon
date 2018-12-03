cdf631f621c7bc42fa89c39ffe3a21fafeef727a
Tobias Johansson <TobiasJohansson911@gmail.com>
2018-12-03 16:04:57 +0100

    ViewModel implemented

---
+++ Labb2OOADSimonOTobias/ViewModels/BaseViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Labb2OOADSimonOTobias.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public BaseViewModel()
+        {
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Object.Equals(storage, value))
+                return false;
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+            return true;
+
+        }
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Labb2OOADSimonOTobias/ViewModels/MainPageVeiwModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Labb2OOADSimonOTobias.ViewModels
+{
+    public class MainPageVeiwModel : BaseViewModel
+    {
+
+        public ICommand PwnedButton { get; set; }
+        private string _inputLabel = "";
+        public string InputLabel 
+        { 
+            get { return _inputLabel; } 
+            set { SetProperty(ref _inputLabel, value); } 
+        }
+        private string _outputLabel = "";
+        public string OutputLabel { 
+            get { return _outputLabel; } 
+            set { SetProperty(ref _outputLabel, value); } 
+        }
+
+        public MainPageVeiwModel()
+        {
+            PwnedButton = new Command(ClickedPwned);
+        }
+
+        private void ClickedPwned()
+        {
+            //Todo: Make text validation
+            //Todo: Make Api call
+        }
+    }
+}
