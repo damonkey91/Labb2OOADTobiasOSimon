d94c12fd7634f0727b3f044cffd643ff24204e8c
Tobias Johansson <TobiasJohansson911@gmail.com>
2018-12-04 10:18:13 +0100

    Validation pattern start

---
+++ Labb2OOADSimonOTobias/ValidationPattern/IValidationRule.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Labb2OOADSimonOTobias.ValidationPattern
+{
+    public interface IValidationRule<T>
+    {
+        string ValidationMessage { get; set; }
+        bool Check(T value);
+    }
+}
